956f74358b1ee7a586327e02fae11b73e56c36bd
Ivan Pozdeev <vano@mail.mipt.ru>
2025-12-15 23:37:08 +0300

    Add tests for other mode indicators

--- tests/test_hashsum.py
+++ tests/test_hashsum.py
@@ -18,6 +18,12 @@ class MyTests(unittest.TestCase):
     with open(os.path.join(THIS_DIR, os.pardir, 'tests/fixtures/centos-7.7/sha384sum.out'), 'r', encoding='utf-8') as f:
         centos_7_7_sha384sum = f.read()
 
+    with open(os.path.join(THIS_DIR, os.pardir, 'tests/fixtures/centos-7.7/sha384sum.out'), 'r', encoding='utf-8') as f:
+        ubuntu_24_04_sha256sum = f.read()
+
+    with open(os.path.join(THIS_DIR, os.pardir, 'tests/fixtures/centos-7.7/sha384sum.out'), 'r', encoding='utf-8') as f:
+        ubuntu_24_04_shasum = f.read()
+
     with open(os.path.join(THIS_DIR, os.pardir, 'tests/fixtures/osx-10.14.6/md5.out'), 'r', encoding='utf-8') as f:
         osx_10_14_6_md5 = f.read()
 
@@ -33,6 +39,12 @@ class MyTests(unittest.TestCase):
 
     with open(os.path.join(THIS_DIR, os.pardir, 'tests/fixtures/centos-7.7/sha384sum.json'), 'r', encoding='utf-8') as f:
         centos_7_7_sha384sum_json = json.loads(f.read())
+
+    with open(os.path.join(THIS_DIR, os.pardir, 'tests/fixtures/ubuntu-24.04/sha256sum.json'), 'r', encoding='utf-8') as f:
+        ubuntu_24_04_sha256sum_json = json.loads(f.read())
+
+    with open(os.path.join(THIS_DIR, os.pardir, 'tests/fixtures/ubuntu-24.04/shasum.json'), 'r', encoding='utf-8') as f:
+        ubuntu_24_04_shasum_json = json.loads(f.read())
 
     with open(os.path.join(THIS_DIR, os.pardir, 'tests/fixtures/osx-10.14.6/md5.json'), 'r', encoding='utf-8') as f:
         osx_10_14_6_md5_json = json.loads(f.read())
@@ -64,6 +76,18 @@ class MyTests(unittest.TestCase):
         Test 'sha384sum' on Centos 7.7
         """
         self.assertEqual(jc.parsers.hashsum.parse(self.centos_7_7_sha384sum, quiet=True), self.centos_7_7_sha384sum_json)
+
+    def test_sha256sum_ubuntu_24_04(self):
+        """
+        Test 'sha256sum' on Ubuntu 24.04
+        """
+        self.assertEqual(jc.parsers.hashsum.parse(self.ubuntu_24_04_sha256sum, quiet=True), self.ubuntu_24_04_sha256sum_json)
+
+    def test_shasum_ubuntu_24_04(self):
+        """
+        Test 'shasum' on Ubuntu 24.04
+        """
+        self.assertEqual(jc.parsers.hashsum.parse(self.ubuntu_24_04_shasum, quiet=True), self.ubuntu_24_04_shasum_json)
 
     def test_md5_osx_10_14_6(self):
         """
